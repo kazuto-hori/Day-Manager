3a9499308f534523f5d38740cbfc91040769e1d1
堀　和人 <hori@horiwajin-no-MacBook-Air.local>
2017-08-23 22:22:10 +0900

    Initial Commit

---
+++ Day Manager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day Manager
+//
+//  Created by 堀　和人 on 2017/08/23.
+//  Copyright © 2017 kazuto.hori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
